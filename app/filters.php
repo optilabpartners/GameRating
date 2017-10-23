38e43ab966c957b1729d3c204138d929fb360945
Hitankar Ray <hitankar@gmail.com>
2017-10-23 21:48:50 +0800

    regenerating term links

--- app/filters.php
+++ app/filters.php
@@ -17,44 +17,6 @@ function the_team_image_taxonomy( $taxonomy ) {
 }
 add_filter( 'taxonomy-term-image-taxonomy', 'the_team_image_taxonomy' );
 
-function filter_game_link($permalink, $post) {
-  if(('game' == $post->post_type) && '' != $permalink && !in_array($post->post_status, array('draft', 'pending', 'auto-draft')) ) {
-	$rewritecode = array(
-	  '%game_org%',
-	  '%game_season%',
-	  '%game%'
-	);
-
-	$game_org = get_the_terms( $post, 'game_org' );
-	if ($game_org !== false) {
-		$game_org = $game_org[0]->slug;
-	} else {
-		$game_org = 'undefined';
-	}
-	
-
-	$game_season_replace = null;
-	$game_season = get_the_terms( $post, 'game_season' );
-
-	if ($game_season !== false) {
-		$game_season_replace = rtrim(get_taxonomy_parents(array_pop($game_season)->term_id, 'game_season', false, '/', true), '/');
-	} else {
-		$game_season_replace = 'undefined';
-	}
-
-	$rewritereplace = array(
-		$game_org,
-		$game_season_replace,
-		$post->post_name
-	);
-
-
-	$permalink = str_replace($rewritecode, $rewritereplace, '/game/%game_org%/%game_season%/%game%/');
-	$permalink = user_trailingslashit(home_url($permalink));
-  }
-  return $permalink;
-}
-
 
 function term_link($url, $term, $taxonomy) {
 
