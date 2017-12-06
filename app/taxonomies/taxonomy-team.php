3932b8ca4160837729d1d3ea855fcf7f5eaf3c03
Hitankar Ray <hitankar@gmail.com>
2017-12-06 15:43:40 +0800

    short name for team

--- app/taxonomies/taxonomy-team.php
+++ app/taxonomies/taxonomy-team.php
@@ -40,3 +40,35 @@ function taxonomy_team() {
 	register_taxonomy( 'team', array( 'game' ), $args );
 }
 add_action( 'init', __NAMESPACE__ . '\\taxonomy_team', 0 );
+
+add_action( 'team_add_form_fields', function($taxonomy) {
+	?>
+  <div class="form-field term-short_name-wrap">
+    <label for="short_name"><?php _e( 'Team Short Name', 'sage' ); ?>
+	<input type="text" name="short_name" id="shortName" value="">
+	<p class="description"><?php _e( 'Short name for team','sage' ); ?></p>
+  </div>
+<?php
+});
+
+add_action( 'team_edit_form_fields', function($term) {
+  $short_name = get_term_meta( $term->term_id, 'short_name', true );
+?>
+  <tr class="form-field term-short_name-wrap">
+    <th scope="row"><label for="short_name"><?php _e( 'Team Short Name', 'sage' ); ?></th>
+    <td>
+      <input type="text" name="short_name" id="short_name" value="<?= $short_name; ?>">
+      <p class="description"><?php _e( 'Short name for team','sage' ); ?></p>
+    </td>
+  </tr>
+<?php
+});
+
+/** Save Custom Field Of Category Form */
+add_action( 'created_team', __NAMESPACE__ . '\\team_form_custom_field_save', 10, 2 );
+add_action( 'edited_team', __NAMESPACE__ . '\\team_form_custom_field_save', 10, 2 );
+function team_form_custom_field_save( $term_id, $tt_id ) {
+    if ( isset( $_POST['short_name'] ) ) {
+      update_term_meta( $term_id, 'short_name', $_POST['short_name'] );
+    }
+}
