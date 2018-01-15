918aaf2a338b1b7975083236364d691c934b82ce
Hitankar Ray <hitankar@gmail.com>
2018-01-15 13:38:54 +0800

    cosmetic changes to improve the look od dates in ios devices

--- app/helpers.php
+++ app/helpers.php
@@ -289,8 +289,8 @@ add_shortcode( 'filter_game', function($atts) {
                 $content .= '</div>';
 
                 $content .= '<div class="col-lg-4 col-sm-6">';
-                $gd = get_query_var('game_date');
-				$content .= "<input type=\"text\" onfocus=\"(this.type='date')\" name=\"game_date\" id=\"gameDate\" placeholder=\"Choose Date\" value=\"{$gd}\" class=\"form-control w-100 mt-1\">";
+                $gd = (get_query_var('game_date'))? get_query_var('game_date'): 'Choose Date' ;
+				$content .= "<input type=\"date\"  name=\"game_date\" id=\"gameDate\" placeholder=\"Choose Date\" value=\"{$gd}\" class=\"form-control w-100 mt-1\">";
 
                 $content .= '</div>';
                 $content .= '<div class="col-lg-4 col-sm-6">';
